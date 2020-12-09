c80c6012ba6d7dfefbf64fe356a900a3ef5214b9
Manage0 <61554465+Manage0@users.noreply.github.com>
2020-12-09 11:31:14 +0100

    Stylization vol2

--- src/CatPurr.js
+++ src/CatPurr.js
@@ -1,18 +1,23 @@
 import ReactAudioPlayer from 'react-audio-player';
 import soundfile from"./catpurr.mp3"
+import "./catpurr.css"
 
 const CatPurr=()=>{
     return(
-        <div>
-            <h2>
+        <div><div className="styleddiv">
+            <b>
                 Here you can hear a cat purr!
-            </h2>
+            </b>
+            </div>
             <br/>
+        <div>
         <ReactAudioPlayer
             src={soundfile}
             controls
         />
         </div>
+        </div>
+        
     )
 }
 export default CatPurr
--- src/Routes/RandomCatThings.js
+++ src/Routes/RandomCatThings.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { useState,useEffect } from 'react'
-import {css} from "styled-components"
+import "../catpurr.css"
 
 const RandomCatThings =()=>{
 
@@ -45,7 +45,7 @@ const RandomCatThings =()=>{
         setSave(true)
       }
     return (
-        <div>
+        <div className="styleddiv">
             {(index+1)+". "+JSON.stringify(facts.all[index].text)}
             <br/>
             {(index+2)+". "+JSON.stringify(facts.all[index+1].text)}
@@ -54,21 +54,14 @@ const RandomCatThings =()=>{
             <br/>
             {(index+4)+". "+JSON.stringify(facts.all[index+3].text)}
             <br/>
+            <div className="styleddiv">
             <button variant='primary' onClick={()=>{index>=4?setIndex(index-4):setIndex(index)}}>
-    {index>=4?<h4>Previous {index-3}-{index}</h4>:<h4>No previous available</h4>}</button>
+            {index>=4?<h4>Previous {index-3}-{index}</h4>:<h4>No previous available</h4>}</button>
             {/*handle negative nums and html danger text*/}
             <button variant='primary' onClick={()=>{setIndex(index+4)}}><h4>Next {index+5}-{index+8}</h4></button>
             <br/>
-            <div
-            css={css`
-            width: 100%;
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
-            margin-top: 0px;
-            background-color: green;
-          `}>
+            </div>
+            <div className="styleddiv">
             <button variant='primary' onClick={()=>{EnableSelection()}}><h4>Select items to save</h4></button>
             <button variant='primary' onClick={()=>{Save()}}><h4>Save Selected items</h4></button>
             </div>
